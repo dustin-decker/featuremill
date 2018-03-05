1ef7d3591824e8be5baa1f753bd779953f54141e
Dustin Decker <dustindecker@protonmail.com>
2018-03-04 22:07:08 -0600

    svm out

--- numerical.go
+++ numerical.go
@@ -1,6 +1,13 @@
 package extractor
 
-// TransformNumberical returns scalled feature
-func TransformNumberical(num float32, max float32) float32 {
-	return num / max
+import (
+	"fmt"
+
+	"github.com/spaolacci/murmur3"
+)
+
+// TransformNumerical returns scalled feature
+func TransformNumerical(field string, num float32, max float32) string {
+	featureID := murmur3.Sum32([]byte(uniqueHashPrefixStr + field))
+	return fmt.Sprintf(" %d:%f", featureID, num/max)
 }
