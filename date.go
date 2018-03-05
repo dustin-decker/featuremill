9d4396d5a4d3909e926871d1dc994dc51ca9e7d1
Dustin Decker <dustindecker@protonmail.com>
2018-03-04 23:20:08 -0600

    test and fix TransformDate

--- date.go
+++ date.go
@@ -20,11 +20,11 @@ func TransformDate(field, date string) ([]string, error) {
 
 	dayOfWeek := int(dt.Weekday())
 	dayOfWeekFeatureID := murmur3.Sum32([]byte(uniqueHashPrefixStr + field + "dayOfWeek"))
-	out = append(out, fmt.Sprintf("%d:%d", dayOfWeekFeatureID, dayOfWeek/6))
+	out = append(out, fmt.Sprintf("%d:%f", dayOfWeekFeatureID, float32(dayOfWeek)/6))
 
 	monthOfYear := dt.Month()
 	monthOfYearFeatureID := murmur3.Sum32([]byte(uniqueHashPrefixStr + field + "monthOfYear"))
-	out = append(out, fmt.Sprintf("%d:%d", monthOfYearFeatureID, monthOfYear/12))
+	out = append(out, fmt.Sprintf("%d:%f", monthOfYearFeatureID, float32(monthOfYear)/12))
 
 	return out, nil
 }
---
+++ date_test.go
@@ -0,0 +1,23 @@
+package extractor
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestTransformDate(t *testing.T) {
+
+	text := "2018-03-05T03:12:14"
+
+	expected := []string{
+		"3667193569:0.166667",
+		"210877497:0.250000",
+	}
+
+	got, _ := TransformDate("@timestamp", text)
+
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("unexpected difference: (-got +want)\n%s", diff)
+	}
+}
