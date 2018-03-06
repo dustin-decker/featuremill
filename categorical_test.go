51c60a230ab8850fb8cfd4cc21ad7c07bf4b0f73
Dustin Decker <dustindecker@protonmail.com>
2018-03-05 18:18:38 -0600

    test TransformCategorical

---
+++ categorical_test.go
@@ -0,0 +1,18 @@
+package extractor
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestTransformCategorical(t *testing.T) {
+
+	expected := "2633207889:1"
+
+	got := TransformCategorical("username or something", "sodunn")
+
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("unexpected difference: (-got +want)\n%s", diff)
+	}
+}
