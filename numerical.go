38f9c5614669b62fbfe60f76724cbf0a7928c129
Dustin Decker <dustindecker@protonmail.com>
2018-03-17 12:50:21 -0500

    log scaling

--- numerical.go
+++ numerical.go
@@ -2,12 +2,19 @@ package featuremill
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/spaolacci/murmur3"
 )
 
+// ExtractNumericalMaxMin returns a min/max scalled vector to a deterministic feature ID
+func ExtractNumericalMaxMin(field string, num float32, min float32, max float32) string {
+	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field)))
+	return fmt.Sprintf("%d:%f", featureID, (num-min)/(max-min))
+}
+
 // ExtractNumerical returns a min/max scalled vector to a deterministic feature ID
 func ExtractNumerical(field string, num float32, min float32, max float32) string {
 	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field)))
-	return fmt.Sprintf("%d:%f", featureID, (num-min)/(max-min))
+	return fmt.Sprintf("%d:%f", featureID, float32(math.Log(float64(num-min))/math.Log(float64(max-min))))
 }
--- numerical_test.go
+++ numerical_test.go
@@ -6,9 +6,20 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestExtractNumerical(t *testing.T) {
+func TestExtractNumericalMaxMin(t *testing.T) {
 
 	expected := "359062843:0.839898"
+
+	got := ExtractNumericalMaxMin("error_rate", 88.1, 22, 100.7)
+
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("unexpected difference: (-got +want)\n%s", diff)
+	}
+}
+
+func TestExtractNumerical(t *testing.T) {
+
+	expected := "359062843:0.960035"
 
 	got := ExtractNumerical("error_rate", 88.1, 22, 100.7)
 
